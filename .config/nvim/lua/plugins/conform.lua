8813a71ad3be2eb15ddca749b50c260110b396e6
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-01 01:00:02 +0700

    Setup conform

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        css = { "prettier" },
+        html = { "prettier" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+      },
+    },
+  },
+}
