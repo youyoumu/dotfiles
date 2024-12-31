9498ae34c32409efdbfc6aaa6df39068604646d7
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-01 03:09:23 +0700

    Config neo-tree

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        width = 50,
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
+}
