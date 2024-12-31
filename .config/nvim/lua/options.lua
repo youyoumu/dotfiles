7a0c661af888e7d37b0cbeae47e6ce7d03320e3e
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-12-31 14:52:31 +0700

    Add tiny inline diagnostic

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -10,6 +10,14 @@ require("lspconfig").eslint.setup {
   end,
 }
 
+vim.diagnostic.config { virtual_text = false }
+vim.g.gitblame_set_extmark_options = {
+  priority = 1000,
+}
+vim.g.gitblame_delay = 5000
+vim.g.gitblame_message_template = "󰘬 <author> • <date> • <summary>"
+vim.g.gitblame_message_when_not_committed = "󰘬 ------ "
+
 vim.g.coc_global_extensions = { "coc-markdownlint" }
 vim.opt.scrolloff = 8
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
