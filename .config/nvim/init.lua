7ec862e4912a42de33a4fb30a0511882cbf0a6cc
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-12-31 12:32:50 +0700

    Move init to options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -37,18 +37,3 @@ vim.schedule(function()
 end)
 
 --------------------------------------------------------------------------------
-
-require("lspconfig").eslint.setup {
-  --- ...
-  on_attach = function(client, bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      buffer = bufnr,
-      command = "EslintFixAll",
-    })
-  end,
-}
-
-vim.g.coc_global_extensions = { "coc-markdownlint" }
-vim.opt.scrolloff = 8
-vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
--- vim.opt.relativenumber = true
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,6 +1,16 @@
 require "nvchad.options"
 
--- add yours here!
+require("lspconfig").eslint.setup {
+  --- ...
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+}
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+vim.g.coc_global_extensions = { "coc-markdownlint" }
+vim.opt.scrolloff = 8
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+-- vim.opt.relativenumber = true
