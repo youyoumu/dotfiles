acc11383ab1082858423612e6e1415c0c5edfb68
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-12-31 12:47:42 +0700

    Change trouble keybind

--- .config/nvim/lua/plugins/trouble.nvim.lua
+++ .config/nvim/lua/plugins/trouble.nvim.lua
@@ -5,32 +5,32 @@ return {
     cmd = "Trouble",
     keys = {
       {
-        "<leader>cx",
+        "<leader>dx",
         "<cmd>Trouble diagnostics toggle<cr>",
         desc = "Diagnostics (Trouble)",
       },
       {
-        "<leader>cX",
+        "<leader>dX",
         "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
         desc = "Buffer Diagnostics (Trouble)",
       },
       {
-        "<leader>cs",
+        "<leader>dy",
         "<cmd>Trouble symbols toggle focus=false<cr>",
         desc = "Symbols (Trouble)",
       },
       {
-        "<leader>cl",
+        "<leader>dl",
         "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
         desc = "LSP Definitions / references / ... (Trouble)",
       },
       {
-        "<leader>cL",
+        "<leader>dL",
         "<cmd>Trouble loclist toggle<cr>",
         desc = "Location List (Trouble)",
       },
       {
-        "<leader>cQ",
+        "<leader>dQ",
         "<cmd>Trouble qflist toggle<cr>",
         desc = "Quickfix List (Trouble)",
       },
