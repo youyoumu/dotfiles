938502e6920bdf5c6d2f483b9ccab31dce7d3d0d
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-12-31 13:45:21 +0700

    Add more keybinds

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,10 +7,15 @@ local map = vim.keymap.set
 map("i", "<C-s>", "<Esc><cmd>w<CR>", { desc = "Exit insert mode and save file" })
 map("n", "]c", ":cnext<CR>", { silent = true, desc = "Quick fix next" })
 map("n", "[c", ":cprev<CR>", { silent = true, desc = "Quick fix previous" })
+map("n", "<leader>cq", "<cmd>cclose<CR>", { desc = "Quick fix close" })
+map("n", "[o", "o<Esc>k", { desc = "Add newline below" })
+map("n", "]o", "O<Esc>j", { desc = "Add newline above" })
+
+map("n", "<leader>fr", "<cmd>Telescope lsp_references<CR>", { desc = "Telescope find references" })
 
 map("n", "<leader>cc", function()
   require("tiny-code-action").code_action()
-end, { noremap = true, silent = true })
+end, { noremap = true, silent = true, desc = "Code action" })
 
 map("n", "<leader>fc", "<cmd> Telescope commands <CR>", { desc = "List commands" })
 
