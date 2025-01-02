3b442af54543b1fba93238aeea66909cd23eb42f
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-02 16:15:41 +0700

    Configure ruby

---
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      servers = {
+        ruby_lsp = {
+          enabled = true,
+          init_options = {
+            formatter = "standard",
+            linters = { "standard" },
+          },
+        },
+        rubocop = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
