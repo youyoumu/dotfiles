1b7ac8a95f93c5946cf2526337f3a9509169f633
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-02 18:40:02 +0700

    Add comment box

---
+++ .config/nvim/lua/plugins/comment-box.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "LudoPinelli/comment-box.nvim",
+    keys = {
+      --          ╭─────────────────────────────────────────────────────────╮
+      --          │                          this                           │
+      --          ╰─────────────────────────────────────────────────────────╯
+      { "<leader>db", "<cmd>CBccbox<CR>", desc = "Create centered comment box" },
+      --  ──────────────────────────────── this ─────────────────────────────
+      { "<leader>dl", "<cmd>CBccline<CR>", desc = "Create centered comment line" },
+    },
+  },
+}
