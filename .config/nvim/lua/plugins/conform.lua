48fc135cea5e5b69cbdd8709d85b0da43aefd97b
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-03 15:52:48 +0700

    Configure prettierd

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,10 @@ return {
     opts = {
       formatters_by_ft = {
         ruby = { "standardrb" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
