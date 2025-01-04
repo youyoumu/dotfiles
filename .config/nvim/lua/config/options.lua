690af736e160be19a2a5c9977c3bfd623217b9e9
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-04 18:25:27 +0700

    Add toggleterm

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -13,4 +13,21 @@ if vim.g.neovide then
   vim.g.neovide_padding_right = 10
   vim.g.neovide_padding_left = 10
   vim.g.neovide_cursor_vfx_mode = "sonicboom"
+
+  vim.g.terminal_color_0 = "#45475a"
+  vim.g.terminal_color_1 = "#f38ba8"
+  vim.g.terminal_color_2 = "#a6e3a1"
+  vim.g.terminal_color_3 = "#f9e2af"
+  vim.g.terminal_color_4 = "#89b4fa"
+  vim.g.terminal_color_5 = "#f5c2e7"
+  vim.g.terminal_color_6 = "#94e2d5"
+  vim.g.terminal_color_7 = "#bac2de"
+  vim.g.terminal_color_8 = "#585b70"
+  vim.g.terminal_color_9 = "#f38ba8"
+  vim.g.terminal_color_10 = "#a6e3a1"
+  vim.g.terminal_color_11 = "#f9e2af"
+  vim.g.terminal_color_12 = "#89b4fa"
+  vim.g.terminal_color_13 = "#f5c2e7"
+  vim.g.terminal_color_14 = "#94e2d5"
+  vim.g.terminal_color_15 = "#a6adc8"
 end
