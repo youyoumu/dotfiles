e1b08f9aa7d55328ffaffed96eeaa75f4eb40606
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-04 14:24:57 +0700

    Config nvimcmp supermaven

---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "supermaven-nvim" },
+    opts = function(_, opts)
+      if vim.g.ai_cmp then
+        table.insert(opts.sources, 1, {
+          name = "supermaven",
+          group_index = 1,
+          priority = 100,
+        })
+      end
+    end,
+  },
+}
