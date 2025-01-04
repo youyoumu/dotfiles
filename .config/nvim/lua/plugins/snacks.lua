6eda529630e49fc13f8722958bbd88b74d45102d
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-04 17:00:42 +0700

    Customize indent char

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -3,6 +3,14 @@ return {
     "snacks.nvim",
     opts = {
       scroll = { enabled = false },
+      indent = {
+        indent = {
+          char = "╎",
+        },
+        scope = {
+          char = "╎",
+        },
+      },
     },
   },
 }
