8e52e92a479264eb728fe89c3a1f82a7d90ef5f6
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-06 17:49:55 +0700

    Change debug print to console info

--- .config/nvim/lua/plugins/debugprint.lua
+++ .config/nvim/lua/plugins/debugprint.lua
@@ -1,6 +1,19 @@
+local js_like = {
+  left = 'console.info("',
+  right = '")',
+  mid_var = '", ',
+  right_var = ")",
+}
 return {
   {
     "andrewferrier/debugprint.nvim",
-    opts = {},
+    opts = {
+      filetypes = {
+        ["javascript"] = js_like,
+        ["javascriptreact"] = js_like,
+        ["typescript"] = js_like,
+        ["typescriptreact"] = js_like,
+      },
+    },
   },
 }
