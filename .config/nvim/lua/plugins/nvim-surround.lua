4d78697601d46953ab7ef43c04c64a692a33ea9d
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-14 13:07:56 +0700

    Change nvim surround keybind

--- .config/nvim/lua/plugins/nvim-surround.lua
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -4,6 +4,9 @@ return {
     config = function()
       require("nvim-surround").setup({
         -- Configuration here, or leave empty to use defaults
+        keymaps = {
+          visual_line = "m",
+        },
       })
     end,
   },
