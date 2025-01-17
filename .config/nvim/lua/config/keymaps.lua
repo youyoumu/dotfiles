c930e82f474faa56ef0c5a13fb195d8f1d4a1ee0
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-17 22:48:29 +0700

    Remove toggle term and yanky

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,8 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-local map = vim.keymap.set
-
 vim.keymap.set("n", "gO", "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>")
 vim.keymap.set("n", "go", "<Cmd>call append(line('.'),     repeat([''], v:count1))<CR>")
 
@@ -14,4 +12,4 @@ vim.keymap.set("n", "<leader>dD", '"_D', { desc = "Delete to blackhole register 
 vim.keymap.set("n", "<leader>k", "<cmd>Format<CR>", { desc = "Format code" })
 vim.keymap.set("v", "<leader>k", "<cmd>Format<CR>", { desc = "Format code" })
 
-vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua git_status<CR>", { desc = "Format code" })
+vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua git_status<CR>", { desc = "Git status" })
