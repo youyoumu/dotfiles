19c54c31cf1dcf7cb7ca111f6c582e90330b7126
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-18 00:56:41 +0700

    Add harpoon keybind

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+
+    keys = function()
+      local keys = {
+        {
+          "<leader>a",
+          function()
+            require("harpoon"):list():add()
+          end,
+          desc = "Harpoon File",
+        },
+        {
+          "<leader>h",
+          function()
+            local harpoon = require("harpoon")
+            harpoon.ui:toggle_quick_menu(harpoon:list())
+          end,
+          desc = "Harpoon Quick Menu",
+        },
+      }
+
+      for i = 1, 5 do
+        table.insert(keys, {
+          "" .. i,
+          function()
+            require("harpoon"):list():select(i)
+          end,
+          desc = "Harpoon to File " .. i,
+        })
+      end
+      return keys
+    end,
+  },
+}
