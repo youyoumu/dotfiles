8439e1f67ea932912c50949e4481a84078603919
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-24 10:06:53 +0700

    Switch to snacks picker

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -1,33 +1,35 @@
-local fzf_lua = require("fzf-lua")
+return {}
 
--- Define a command to run zoxide query
-vim.api.nvim_create_user_command("ZoxideFzf", function()
-  fzf_lua.fzf_exec(
-    "zoxide query -l", -- Command to fetch zoxide entries
-    {
-      prompt = "📁 󰜴 ",
-      preview = "eza --color=always --icons=always -a -T -L 1 {}",
-      actions = {
-        ["default"] = function(selected)
-          if selected[1] then
-            vim.cmd("cd " .. selected[1])
-            print("Changed directory to: " .. selected[1])
-          end
-        end,
-      },
-    }
-  )
-end, {})
-
-return {
-  {
-    "ibhagwan/fzf-lua",
-    opts = {
-      winopts = {
-        preview = {
-          delay = 200,
-        },
-      },
-    },
-  },
-}
+-- local fzf_lua = require("fzf-lua")
+--
+-- -- Define a command to run zoxide query
+-- vim.api.nvim_create_user_command("ZoxideFzf", function()
+--   fzf_lua.fzf_exec(
+--     "zoxide query -l", -- Command to fetch zoxide entries
+--     {
+--       prompt = "📁 󰜴 ",
+--       preview = "eza --color=always --icons=always -a -T -L 1 {}",
+--       actions = {
+--         ["default"] = function(selected)
+--           if selected[1] then
+--             vim.cmd("cd " .. selected[1])
+--             print("Changed directory to: " .. selected[1])
+--           end
+--         end,
+--       },
+--     }
+--   )
+-- end, {})
+--
+-- return {
+--   {
+--     "ibhagwan/fzf-lua",
+--     opts = {
+--       winopts = {
+--         preview = {
+--           delay = 200,
+--         },
+--       },
+--     },
+--   },
+-- }
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -14,7 +14,6 @@ return {
       dashboard = {
         preset = {
           keys = {
-            { icon = " ", key = "d", desc = "Change CWD", action = ":ZoxideFzf" },
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
