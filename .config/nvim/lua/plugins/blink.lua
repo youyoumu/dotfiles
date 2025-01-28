c3b3eaf50bfc4e16a65cf67c4883a12824e4584a
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-01-28 18:15:16 +0700

    Switch to blink cmp

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,36 +1,11 @@
 return {
   {
     "saghen/blink.cmp",
-    optional = true,
     opts = {
-      sources = {
-        default = { "luasnip" },
-        compat = { "supermaven" },
-        providers = {
-          supermaven = {
-            kind = "Supermaven",
-            score_offset = 100,
-            async = true,
-          },
-        },
-        cmdline = {
-          name = "cmdline",
-          module = "blink.cmp.sources.cmdline",
-        },
-      },
-      snippets = {
-        expand = function(snippet)
-          require("luasnip").lsp_expand(snippet)
-        end,
-        active = function(filter)
-          if filter and filter.direction then
-            return require("luasnip").jumpable(filter.direction)
-          end
-          return require("luasnip").in_snippet()
-        end,
-        jump = function(direction)
-          require("luasnip").jump(direction)
-        end,
+
+      keymap = {
+        preset = "default",
+        ["<S-CR>"] = { "select_and_accept" },
       },
     },
   },
