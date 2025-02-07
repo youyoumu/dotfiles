ce701d8e18687f56972d7eb6cef568b89785d3c3
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-02-07 13:01:18 +0700

    Configure flash

---
+++ .config/nvim/lua/plugins/flash.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "folke/flash.nvim",
+    opts = {
+      label = {
+        uppercase = false,
+      },
+      search = {
+        multi_window = false,
+        mode = "fuzzy",
+      },
+      jump = {
+        pos = "end",
+      },
+      modes = {
+        char = {
+          highlight = {
+            backdrop = false,
+          },
+        },
+      },
+    },
+  },
+}
