32053f719b4b8770bb345add94d5d0d0f722cc13
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-02-18 12:46:16 +0700

    Configure noice

---
+++ .config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      routes = {
+        {
+          view = "notify",
+          filter = { event = "msg_showmode" },
+        },
+      },
+    },
+  },
+}
