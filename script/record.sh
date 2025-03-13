3ea7260fabf02464daf41899f961fe645ad35de0
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-03-13 22:58:29 +0700

    Bind record script

--- script/record.sh
+++ script/record.sh
@@ -9,12 +9,17 @@ FILENAME="recording_$(date '+%Y-%m-%d_%H-%M-%S').mp4"
 FILEPATH="$OUTPUT_DIR/$FILENAME"
 
 # Show a selection border using grim before slurp
+
+pgrep -x "wf-recorder" && pkill -INT -x wf-recorder && notify-send -h string:wf-recorder:record -t 1000 "Finished Recording" && exit 0
+
+notify-send -h string:wf-recorder:record -t 1000 "Recording started"
+
 REGION=$(slurp -b 00000055) # 55 is transparency (00-FF)
 
 # If a region was selected, start recording
 if [ -n "$REGION" ]; then
   echo "Recording started... Press Ctrl+C to stop."
-  wf-recorder -g "$REGION" -f "$FILEPATH"
+  wf-recorder -g "$REGION" -f "$FILEPATH" -r 24
   echo "Recording saved as $FILEPATH"
 else
   echo "No region selected. Exiting."
