498b9a8858846909ceceb123f82495dcb6147b12
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-06-02 14:34:48 +0700

    Fix gstreamer

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -284,6 +284,17 @@
 
   environment.variables.EDITOR = "nvim";
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.variables.GST_PLUGIN_SYSTEM_PATH_1_0 = pkgs.lib.mkForce (
+    pkgs.lib.concatStringsSep ":" [
+      "${pkgs.gst_all_1.gstreamer}/lib/gstreamer-1.0"
+      "${pkgs.gst_all_1.gst-plugins-base}/lib/gstreamer-1.0"
+      "${pkgs.gst_all_1.gst-plugins-good}/lib/gstreamer-1.0"
+      "${pkgs.gst_all_1.gst-plugins-bad}/lib/gstreamer-1.0"
+      "${pkgs.gst_all_1.gst-plugins-ugly}/lib/gstreamer-1.0"
+      "${pkgs.gst_all_1.gst-libav}/lib/gstreamer-1.0"
+      "${pkgs.gst_all_1.gst-vaapi}/lib/gstreamer-1.0"
+    ]
+  );
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
