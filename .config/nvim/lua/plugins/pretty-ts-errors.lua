d59edbf3e50273424abc27eb7e0942c3b766b684
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-06-07 22:55:07 +0700

    Configure walker

--- .config/nvim/lua/plugins/pretty-ts-errors.lua
+++ .config/nvim/lua/plugins/pretty-ts-errors.lua
@@ -1,6 +1,7 @@
 return {
   "youyoumu/pretty-ts-errors.nvim",
   name = "pretty-ts-errors.nvim",
+  -- dir = "~/repos/pretty-ts-errors.nvim-vaspadi/",
   dev = false,
   opts = {
     -- your configuration options
