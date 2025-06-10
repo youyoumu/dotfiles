19594878f321ef0b50bdcf16e673669ce94cdc83
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-06-10 13:48:02 +0700

    Add Niri

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -97,9 +97,10 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
   programs.hyprland.enable = true; # enable Hyprland
+  programs.niri.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
@@ -182,8 +183,7 @@
       "docker"
       "uinput"
     ];
-    packages = with pkgs; [
-      #  thunderbird
+    packages = [
     ];
   };
 
