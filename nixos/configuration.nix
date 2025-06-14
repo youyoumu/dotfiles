8d1928deafc46e00790703dc3c2c3d3f452182ba
youyoumu <19760884+youyoumu@users.noreply.github.com>
2025-06-14 18:35:33 +0700

    Move docker

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -34,14 +34,24 @@
     device = "/dev/disk/by-uuid/2393FC547EB4A8F5";
     fsType = "auto";
     options = [
+      "nosuid" # Prevents execution of set-user-identifier (SUID) or set-group-identifier (SGID) binaries.
+      "nodev" # Blocks interpretation of device files (like /dev/sda, /dev/null) on that filesystem.
+      "nofail" # Don't fail the boot if the filesystem is missing or cannot be mounted.
+      "x-gvfs-show" # A special flag for GNOME and other GVFS-based file managers (like Nautilus).
+    ];
+  };
+  fileSystems."/mnt/SSD-1TB" = {
+    device = "/dev/disk/by-uuid/6C0ACF540ACF19CA";
+    fsType = "auto";
+    options = [
       "nosuid"
       "nodev"
       "nofail"
       "x-gvfs-show"
     ];
   };
-  fileSystems."/mnt/SSD-1TB" = {
-    device = "/dev/disk/by-uuid/6C0ACF540ACF19CA";
+  fileSystems."/mnt/SSD-1TB-128GB" = {
+    device = "/dev/disk/by-uuid/9947e5d5-57da-4bec-9351-4c56c0745d77";
     fsType = "auto";
     options = [
       "nosuid"
@@ -195,7 +205,12 @@
     # clean.enable = true;
     # clean.extraArgs = "--keep-since 4d --keep 3";
   };
-  virtualisation.docker.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      data-root = "/mnt/SSD-1TB-128GB/chocola/var/lib/docker";
+    };
+  };
   programs.gnupg.agent = {
     enable = true;
     # enableSSHSupport = true;
@@ -319,6 +334,7 @@
     satty
     grim
     adw-gtk3
+    amdgpu_top
   ];
 
   fonts.packages = with pkgs; [
