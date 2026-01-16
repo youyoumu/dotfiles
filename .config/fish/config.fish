930925cf525492d3437bc54a1695c022c76226b1
youyoumu <19760884+youyoumu@users.noreply.github.com>
2026-01-16 23:38:37 +0700

    Update fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,38 +1,27 @@
 fish_vi_key_bindings
-
 set -g fish_greeting
-
+fish_config theme choose "Catppuccin Mocha"
 starship init fish | source
-
-# zoxide
 zoxide init fish | source
 
 # eza
 alias list="command ls"
 alias ls="eza --long --icons --git --all --header --binary --no-permissions \
-              --no-user --mounts --grid --group-directories-first"
-alias lsl="eza --long --icons --header --all --binary --mounts --group-directories-first \
-               --group"
+          --no-user --mounts --grid --group-directories-first"
+alias lsl="eza --long --icons --header --all --binary --mounts \
+          --group-directories-first --group"
 alias lt="eza --tree --icons --all --group-directories-first"
+alias lt1="eza --tree --level=2 --icons --all --group-directories-first"
 alias lt2="eza --tree --level=2 --icons --all --group-directories-first"
 alias lt3="eza --tree --level=3 --icons --all --group-directories-first"
 alias lt4="eza --tree --level=4 --icons --all --group-directories-first"
 alias lt5="eza --tree --level=5 --icons --all --group-directories-first"
 
-# sesh
 alias s="sesh connect (sesh list | fzf)"
-
-# lazygit
 alias lg="lazygit"
-
-# nvim
 alias n="nvim"
 alias nd="neovide &"
-
-# pnpm
 alias p="pnpm"
-
-# just
 alias j="just"
 
 # yazi
